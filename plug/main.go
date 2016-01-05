38af3b814e1390b4a1849fa27d70cd14daeb2161
mijime <07cc0110+gthb@gmail.com>
2016-01-05 14:22:20 +0900

    :art: Implement the behavior of a plug

---
+++ plug/main.go
@@ -0,0 +1,87 @@
+package plug
+
+import (
+	"golang.org/x/tools/go/vcs"
+	"os"
+	"path/filepath"
+)
+
+type Plug struct {
+	dir  string
+	rev  string
+	repo string
+	on   []string
+	use  []string
+	of   []string
+	at   []string
+	do   string
+}
+
+func (p *Plug) Sync() error {
+	if p.isExists() {
+		return p.update()
+	}
+
+	return p.install()
+}
+
+func (p *Plug) Check() {
+}
+
+func (p *Plug) Remove() {
+}
+
+func (p *Plug) getPath(path string) string {
+	dir, _ := filepath.Abs(filepath.Join(p.dir, path))
+	return dir
+}
+
+func (p *Plug) fetchRoot() (*vcs.RepoRoot, error) {
+	return vcs.RepoRootForImportPath(p.repo, false)
+}
+
+func (p *Plug) isExists() bool {
+	dir := p.getPath("")
+	_, err := os.Stat(dir)
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (p *Plug) install() error {
+	r, err := p.fetchRoot()
+
+	if err != nil {
+		return err
+	}
+
+	dir := p.getPath("")
+	parentDir := p.getPath("..")
+
+	err = os.Mkdir(parentDir, 0755)
+
+	if err != nil {
+		return err
+	}
+
+	return r.VCS.CreateAtRev(dir, r.Repo, p.rev)
+}
+
+func (p *Plug) update() error {
+	r, err := p.fetchRoot()
+
+	if err != nil {
+		return err
+	}
+
+	dir := p.getPath("")
+
+	if err := r.VCS.TagSync(dir, p.rev); err != nil {
+		return err
+	}
+
+	return r.VCS.Download(dir)
+}
---
+++ plug/main_test.go
@@ -0,0 +1,17 @@
+package plug
+
+import "testing"
+
+func TestPlugSync(t *testing.T) {
+	plug := &Plug{
+		dir:  "_test/sham",
+		repo: "github.com/mijime/sham",
+	}
+
+	err := plug.Sync()
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
